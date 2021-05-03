18c8f4b38156e528a5a8f6b0e9c2f428bf7832cf
unknown <geovonnnievasquez@gmail.com>
2021-05-03 15:42:29 -0500

    jumbotron

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
 import "./styles/app.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
+import {Jumbotron, Button} from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-     <h1>Dungeon King</h1>
+      <h1 className="title">Dungeon King</h1>
+      {/*  */}
+      <div className="container">
+      <Jumbotron>
+        <h1 className="intro-head">Escape the Dungeon</h1>
+        <p>
+          This is a simple hero game, you are Tavious a 
+        </p>
+        <p>
+          <Button variant="primary">Start</Button>
+        </p>
+      </Jumbotron>
+
+      </div>
     </div>
   );
 }
