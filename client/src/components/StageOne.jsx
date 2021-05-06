be9b4a214c54b27bc5f63a43ba5e1f3615cd8a92
unknown <geovonnnievasquez@gmail.com>
2021-05-06 09:05:49 -0500

    answer update

--- client/src/components/StageOne.jsx
+++ client/src/components/StageOne.jsx
@@ -1,27 +1,25 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Jumbotron, Button } from "react-bootstrap";
-import {useHistory} from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 const StageOne = () => {
-
   let history = useHistory();
 
   // State
-  const [userAnswer, setUserAnswer] = useState(""); 
+  const [userAnswer, setUserAnswer] = useState("");
 
- const getAnswerValue = (e) => {
-    setUserAnswer(e.target.value)
+  const getAnswerValue = (e) => {
+    setUserAnswer(e.target.value);
   };
 
- const checkAnswer = (e) => {
-   e.preventDefault()
-   if(userAnswer === "juniper sword"){
-      alert("yay")
-   }else{
-      history.push("/")
-   }
- };
-
+  const checkAnswer = (e) => {
+    e.preventDefault();
+    if (userAnswer === "juniper-sword") {
+      alert("yay");
+    } else {
+      history.push("/");
+    }
+  };
 
   // JSX
   return (
@@ -33,26 +31,51 @@ const StageOne = () => {
           <h1 className="intro-head">The Weaponary</h1>
           {/* qusetion */}
           <p>
-            A warrior is as great as his sword. You walk into a room that appears to be one of a black smiths.
-
+            A warrior is as great as his sword. You walk into a room and find a
+            wall of armory with a
           </p>
           {/* user selection */}
-          <form onSubmit={checkAnswer} >
-            <input
-              type="radio"
-              id="age1"
-              name="age"
-              value="30"
-              style={{ color: "black" }}
-              onClick={getAnswerValue}
-            />
-            <label for="age1">0 - 30</label>
-            <br></br>
-          <Button variant="primary" type="submit" className="start-btn">
-            Start
-          </Button>
+          <form onSubmit={checkAnswer}>
+            <div className="answer-div">
+              {/* answer one  */}
+              <input
+                type="radio"
+                id="lightning-sword"
+                name="age"
+                value="lightning-sword"
+                style={{ color: "black" }}
+                onClick={getAnswerValue}
+              />
+              <label for="lightning-sword">Lightning Sword</label>
+              <br></br>
+              {/* answer two */}
+              <input
+                type="radio"
+                id="juniper-sword"
+                name="age"
+                value="juniper-sword"
+                style={{ color: "black" }}
+                onClick={getAnswerValue}
+              />
+              <label for="juniper-sword">Juniper Sword</label>
+              <br></br>
+              {/* answer three */}
+              <input
+                type="radio"
+                id="earth-staff"
+                name="age"
+                value="earth-staff"
+                style={{ color: "black" }}
+                onClick={getAnswerValue}
+              />
+              <label for="earth-staff">Earth Staff</label>
+              <br></br>
+            </div>
+            <Button variant="primary" type="submit" className="start-btn">
+              Start
+            </Button>
           </form>
-         {/* user selection end */}
+          {/* user selection end */}
         </Jumbotron>
         {/*  */}
       </div>
